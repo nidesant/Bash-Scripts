2981e1cde4ab1e5fadd88472c61eb2d0187945cf
Nicholas DeSantis <Nick@Nicholass-MacBook-Pro-2.local>
2016-08-22 21:23:21 -0400

    Initial commit.

---
+++ user_check.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#This script will check if the specified user has been added to the servers specified in the ips.txt file by checking /etc/passwd. Results will be written to a log file called check_user.log with the host, username, date, and time. 
+
+#Wriiten by Nick DeSantis
+
+IP=`cat ips.txt`
+now=$(date '+%d-%m-%y')
+t=$(date '+%T')
+
+echo "Please enter a user:"
+read user
+
+for i in ${IP};
+do
+echo '------------------------------------------------------------';
+echo "Connecting to $i";
+echo "";
+STATUS=`ssh -o StrictHostKeyChecking=no -o ConnectTimeout=5 -o PasswordAuthentication=no $i "cat /etc/passwd" | grep "$user";`
+if [ -z $STATUS ];
+then
+echo "The user does not appear to be on the system";
+echo "${i} ${user} ${now} ${t}" >> check_user.log;
+fi
+echo $STATUS;
+echo '------------------------------------------------------------';
+echo "";
+done;
