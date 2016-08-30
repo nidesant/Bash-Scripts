beb07102ee4efbc9ec1315b689ac47283d94f286
Nicholas DeSantis <Nick@Nicholass-MacBook-Pro-2.local>
2016-08-29 21:27:33 -0400

    fixed env variable for

--- diskusage.sh
+++ diskusage.sh
@@ -4,8 +4,8 @@ admin="nidesant@gmail.com"
 threshold="85"
 
 df -h | tail -n +2 | awk '{print $1 " " $5}' | sed '/.//' | while read i; do
-  part=$(echo $1 | awk '{print $1}'
-  util=$(echo $5 | awk '{print $5}'
+  part=$(echo $i | awk '{print $1}'
+  util=$(echo $i | awk '{print $5}'
     if [ $util -ge $ $threshold}; then
       echo $part is %{util}% full. An email has been sent to your administrator."
       mail -s "Critical Alert: $part is ${util}% full. Clear space immeadiately" $admin
