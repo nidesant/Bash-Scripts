a489821ea4613458b24b294c2a3e363d5ffa349b
Nicholas DeSantis <Nick@Nicholass-MacBook-Pro-2.local>
2016-08-29 21:33:24 -0400

    fixed sed syntax

--- diskusage.sh
+++ diskusage.sh
@@ -3,11 +3,11 @@
 admin="nidesant@gmail.com"
 threshold="85"
 
-df -h | tail -n +2 | awk '{print $1 " " $5}' | sed '/.//' | while read i; do
-  part=$(echo $i | awk '{print $1}'
-  util=$(echo $i | awk '{print $5}'
-    if [ $util -ge $ $threshold}; then
-      echo $part is %{util}% full. An email has been sent to your administrator."
+df -h | tail -n +2 | awk '{print $1 " " $5}' | sed 's/.$//' | while read i; do
+  part=$(echo $i | awk '{print $1}')
+  util=$(echo $i | awk '{print $5}')
+    if [ $util -ge $threshold ]; then
+      echo $part is %{util}% full. An email has been sent to your administrator.
       mail -s "Critical Alert: $part is ${util}% full. Clear space immeadiately" $admin
     fi
 done
