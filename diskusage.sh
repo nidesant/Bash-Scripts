ceb1821b4b53ec7c076fab2f4029e5fa261e0288
Nick DeSantis <nidesant@gmail.com>
2016-09-06 09:50:38 -0400

    modified $admin variable

--- diskusage.sh
+++ diskusage.sh
@@ -7,7 +7,7 @@ when disk space is 85% and over.
 
 #Written by Nick DeSantis
 
-admin="nidesant@gmail.com"
+admin="foo@bar.com"
 threshold="85"
 
 df -h | tail -n +2 | awk '{print $1 " " $5}' | sed 's/.$//' | while read i; do
