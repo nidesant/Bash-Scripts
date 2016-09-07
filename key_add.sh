25274ed59e4fd30abd11b11a91bc4ca27a86b80e
Nick DeSantis <nidesant@gmail.com>
2016-09-07 11:56:24 -0400

    removed unnecessary echo command after $dir_check

--- key_add.sh
+++ key_add.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
 #This script will add your public key to ~/.ssh/authorized_keys on the specified remote server for key based authentication. 
-#Before executing, make sure permissions are correct locally and on the remote server.
-#'chmod 700 .ssh' and 'chmod 640 .ssh/authorized_keys'
+#Script will check if .ssh directory and authorized_keys file exists on the remote server.
+#It will also set correct permissions locally and  on the remote server i.e. 'chmod 700 .ssh' and 'chmod 640 .ssh/authorized_keys'
 #This script assumes you have already generated an ssh key pair on your local machine.
+
+#authorized_keys might also be named id_rsa.pub depending on how you generated your key pair
+chmod 700 ~/.ssh; chmod 640 ~/.ssh/authorized_keys
 
 echo Please enter IP or hostname of remote host:
 read host
 
-#Replace user with your username
-dir_check=`ssh nidesant@$host 'ls -al | grep .ssh'`
-echo $dir_check
+#Replace 'user' with your username
+dir_check=`ssh user@$host 'ls -al | grep .ssh'`
 
-if [ -z $dir_check ]; then
-  `ssh nidesant@$host 'mkdir -p .ssh; chmod 700 .ssh; cd .ssh/; touch authorized_keys; chmod 640 authorized_keys'`
+if [[ -z $dir_check ]]; then
+  echo ".ssh directory or authorized_keys file doesn't appear to be present. Re-enter your password for directory and/or file to be created."
+  `ssh user@$host 'mkdir -p .ssh; chmod 700 .ssh; cd .ssh/; touch authorized_keys; chmod 640 authorized_keys'`
   echo "Directory .ssh and authorized_keys file have been created!"
+  echo "Please re-enter your password one more time for your key to be uploaded"
 else
   echo "~/.ssh/authorized_keys already exists. You will be prompted once more for your password and your key will be uploaded!"
 fi
 
-`cat ~/.ssh/id_rsa.pub | ssh nidesant@$host 'cat >> .ssh/authorized_keys'`
-echo "Your public key has been added to the remote server!"
+#This path might vary and need to be changed based on your local path i.e. your public key might be named id_rsa.pub instead of authroized_keys or vice versa
+`cat ~/.ssh/authorized_keys | ssh user@$host 'cat >> .ssh/authorized_keys'`
+echo "Your public key has been added to the remote server! You should now be able to login without a password!"
 
 
 
