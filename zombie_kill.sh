a6a97dd9dd60ffd435e5a0c56153d921e54b94fa
Nick DeSantis <nidesant@gmail.com>
2016-09-15 15:29:07 -0400

    added comments

--- zombie_kill.sh
+++ zombie_kill.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-z=`ps aux | awk '{print $2 " " $8}' | grep 'Z'
-echo $z
+#Will kill all zombie proccesses by sending SIGCHLD signal to parent process to execute wait() system call. 
+#If the parent process ignores the SIGCHLD signal, the parent process will be killed and restarted.
+#Note that if the parent PID is init (1), the process cannot be killed. 
+
+z=`ps aux | awk '{print $2 " " $8}' | grep 'Z'`
