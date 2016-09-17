9136326423c934d0f387527977290eda9d0059f9
c_ndesan <Nicholas.Desantis@RalphLauren.com>
2016-09-17 00:09:38 -0400

    changed ps flags

--- zombie_kill.sh
+++ zombie_kill.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-z=`ps aux | awk '{print $2 " " $8}' | grep 'Z'
+#Will kill all zombie proccesses by sending SIGCHLD signal to parent process to execute wait() system call. 
+#If the parent process ignores the SIGCHLD signal, the parent process will be killed and restarted.
+#Note that if the parent PID is init (1), the process cannot be killed. 
+
+ps -elf | awk '{print $2 " " $4 " " $5}'
+
