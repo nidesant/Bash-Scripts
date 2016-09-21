ec9b4cb258908fd611a01781563ab4df54c79f9b
c_ndesan <Nicholas.Desantis@RalphLauren.com>
2016-09-21 13:47:50 -0400

    added kill -s command

--- zombie_kill.sh
+++ zombie_kill.sh
@@ -2,10 +2,23 @@
 
 #Will kill all zombie proccesses by sending SIGCHLD signal to parent process to execute wait() system call. 
 #If the parent process ignores the SIGCHLD signal, the parent process will be killed and restarted.
-#Note that if the parent PID is init (1), the process cannot be killed. 
+#Note that if the parent PID is init or systemd (1), the process cannot be killed. 
 
-z=`ps -elf | awk '{print $2 " " $4 " " $5}' | grep 'Z'`
+z=`ps -elf | awk '{print $2 " " $4 " " $5}' | tail -n +2 | grep 'Z'`
 
 if [ -z $z ]; then 
   echo "There are currently no zombie processes!"
-fi
+else
+  echo "Zombies found, trying to kill zombies!"
+  k=$(echo $z | awk '{print $5}')
+  if [ $k == 1 ]; then
+    echo "PPID is 1 (init or systemd), and cannot be killed. If necessary, reboot the server."
+  else
+    kill -s SIGCHLD $k 
+    echo "Sending SIGCHLD signal to parent process..."
+    if [ -z $k ]; then
+      echo "Zombies have been killed!"
+    else
+      echo "Not able to kill all zombies!"
+fi  
+   
