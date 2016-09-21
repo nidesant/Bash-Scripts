82b3f6de16ae4a19d118978ab28fb1b0ea9211e6
Nick DeSantis <nidesant@gmail.com>
2016-09-21 14:06:50 -0400

    changed if else logic

--- zombie_kill.sh
+++ zombie_kill.sh
@@ -5,21 +5,22 @@
 #Note that if the parent PID is init or systemd (1), the process cannot be killed. 
 
 z=`ps -elf | awk '{print $2 " " $4 " " $5}' | tail -n +2 | grep 'Z'`
+k=$(echo $z | awk '{print $5}')
 
 if [ -z $z ]; then 
   echo "There are currently no zombie processes!"
 else
   echo "Zombies found, trying to kill zombies!"
-  k=$(echo $z | awk '{print $5}')
-  if [ $k == 1 ]; then
-    echo "PPID is 1 (init or systemd), and cannot be killed. If necessary, reboot the server."
-  else
-    kill -s SIGCHLD $k 
-    echo "Sending SIGCHLD signal to parent process..."
-  fi
-    if [ -z $k ]; then
-      echo "Zombies have been killed!"
-    else
-      echo "Not able to kill all zombies!"
+fi
+if [ $k == 1 ]; then
+  echo "PPID is 1 (init or systemd), and cannot be killed. If necessary, reboot the server."
+else
+  kill -s SIGCHLD $k 
+  echo "Sending SIGCHLD signal to parent process..."
+fi
+if [ -z $k ]; then
+  echo "Zombies have been killed!"
+else
+    echo "Not able to kill all zombies!"
 fi  
    
