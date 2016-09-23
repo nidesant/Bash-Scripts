b7be6ec86d69056c15922ca570ca123f535d140b
c_ndesan <Nicholas.Desantis@RalphLauren.com>
2016-09-22 21:51:04 -0400

    fixed awk statement in

--- zombie_kill.sh
+++ zombie_kill.sh
@@ -5,22 +5,22 @@
 #Note that if the parent PID is init or systemd (1), the process cannot be killed. 
 
 z=`ps -elf | awk '{print $2 " " $4 " " $5}' | tail -n +2 | grep 'Z'`
-k=$(echo $z | awk '{print $5}')
+k=$(echo $z | awk '{print $3}')
 
-if [[ -z $z ]]; then 
+if [[ -z $z ]]; then
   echo "There are currently no zombie processes!"
 else
   echo "Zombies found, trying to kill zombies!"
 if [ "$k" == 1 ]; then
   echo "PPID is 1 (init or systemd), and cannot be killed. If necessary, reboot the server."
 else
-  kill -s SIGCHLD $k 
+  kill -s SIGCHLD $k
   echo "Sending SIGCHLD signal to parent process..."
 fi
-if [ -z $k ]; then
+if [[ -z $k ]]; then
   echo "Zombies have been killed!"
 else
-    echo "Not able to kill all zombies!"
+    echo "Parent process ignored the signal, killing parent process!"
     kill -9 $k
-fi   
+fi
 fi
