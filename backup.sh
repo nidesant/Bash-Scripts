4c81a6bd7f9069733be4d9f6e11f3a95ae1ec84d
Nick DeSantis <nidesant@gmail.com>
2016-09-27 12:30:11 -0400

    fixed syntax line 7

--- backup.sh
+++ backup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#Takes a daily backup of /var/log/messages for archival purposes. 
+#Takes a daily backup of /var/log/messages for archival purposes. Has to be run as root.
 #Script uses cd command instead of absolute paths for security reasons. 
 #Will send email to $admin if backup is successful.
 
-admin=foo@bar.com
+admin="foo@bar.com"
 of=messages-backup-$(date +%Y%m%d).tgz
 
 cd /var/log/
