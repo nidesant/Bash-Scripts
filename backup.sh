e539618b4577909798f38457abb1157506c16e70
Nick DeSantis <nidesant@gmail.com>
2016-09-30 16:39:25 -0400

    removed user and IP of remote server

--- backup.sh
+++ backup.sh
@@ -12,7 +12,7 @@ dir_five=home-backup-$(date +%Y%m%d).tgz
 
 cd /tmp/
 
-tar -czvf $dir_one /etc/; scp $dir_one root@54.245.32.2:/mnt/backup/; rm -rf $dir_one
+tar -czvf $dir_one /etc/; scp $dir_one <user>@<remote-host>:/mnt/backup/; rm -rf $dir_one
 
 if [ $? -eq 0 ]; then
   echo Success | mail -s "Backup of /etc was successful!" $admin
@@ -20,7 +20,7 @@ else
   echo Failure | mail -s "Backup of /etc has failed!" $admin
 fi
 
-tar -czvf $dir_two /boot/; scp $dir_two root@54.245.32.2:/mnt/backup/; rm -rf $dir_two
+tar -czvf $dir_two /boot/; scp $dir_two <user>@<remote-host>:/mnt/backup/; rm -rf $dir_two
 
 if [ $? -eq 0 ]; then
   echo Success | mail -s "Backup of /boot was successful!" $admin
@@ -28,7 +28,7 @@ else
   echo Failure | mail -s "Backup of /boot has failed!" $admin
 fi
 
-tar -czvf $dir_three /usr/local/; scp $dir_three root@54.245.32.2:/mnt/backup/; rm -rf $dir_three
+tar -czvf $dir_three /usr/local/; scp $dir_three <user>@<remote-host>:/mnt/backup/; rm -rf $dir_three
 
 if [ $? -eq 0 ]; then
   echo Success | mail -s "Backup of /usr/local was successful!" $admin
@@ -36,7 +36,7 @@ else
   echo Failure | mail -s "Backup of /usr/local has failed!" $admin
 fi
 
-tar -czvf $dir_four /root/; scp $dir_four root@54.245.32.2:/mnt/backup/; rm -rf $dir_four
+tar -czvf $dir_four /root/; scp $dir_four <user>@<remote-host>:/mnt/backup/; rm -rf $dir_four
 
 if [ $? -eq 0 ]; then
   echo Success | mail -s "Backup of /root was successful!" $admin
@@ -44,7 +44,7 @@ else
   echo Failure | mail -s "Backup of /root has failed!" $admin
 fi
 
-tar -czvf $dir_five /home/; scp $dir_five root@54.245.32.2:/mnt/backup/; rm -rf $dir_five
+tar -czvf $dir_five /home/; scp $dir_five <user>@<remote-host>:/mnt/backup/; rm -rf $dir_five
 
 if [ $? -eq 0 ]; then
   echo Success | mail -s "Backup of /home was successful!" $admin
